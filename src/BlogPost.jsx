adf1c09f16c6d94b12598de2814680e9fe9dea67
Rodrigo Blanco <rodrigozx@hotmail.com>
2023-07-29 16:59:40 -0300

    how to use useNavigate

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { blogdata } from "./blogdata";
 
 function BlogPost() {
   const { slug } = useParams();
+  const navigate = useNavigate();
 
   const blogpost = blogdata?.find((post) => post.slug === slug);
+
+  const returnToBlog = () => navigate("/blog");
+
   return (
     <>
       <h1>{blogpost.title}</h1>
+      <button onClick={returnToBlog}>Volver a los blogs</button>
       <p>{blogpost.content}</p>
       <p>{blogpost.author}</p>
     </>
